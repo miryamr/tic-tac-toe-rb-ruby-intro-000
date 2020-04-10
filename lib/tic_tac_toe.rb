99dba312339a4363bf3dbeb5ce6de927ca38b406
Miryam Rabinowitz <miryamrabinowitz@gmail.com>
2020-04-10 20:46:58 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -100,7 +100,7 @@ def play(board)
       turn(board)
   end
     if won?(board)
-      puts "Congratulations #{winner(board)}"
+      puts "Congratulations #{winner(board)}!"
     else
       puts "Cat's Game!"
     end
