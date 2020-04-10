0cb053c421f703c660af99d28527f6e52979ac67
Miryam Rabinowitz <miryamrabinowitz@gmail.com>
2020-04-10 19:49:37 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -38,10 +38,10 @@ def turn(board)
   puts "Please enter 1-9:"
   user_input = gets.strip
   index = input_to_index(user_input)
-  character = "X" || character = "O"
+#  character = "X" || character = "O"
 binding.pry
   if valid_move?(board, index)
-    return move(board, index, character)
+    return move(board, index, current_player)
   else
     turn(board)
   end
